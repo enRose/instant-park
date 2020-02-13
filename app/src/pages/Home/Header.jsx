3b8f962fdd615036bd8a12a232b93f2bcec7234a
Yini Yin <yiniski@outlook.com>
2020-02-13 17:07:19 +1300

    home page header logo and cta button

--- app/src/pages/Home/Header.jsx
+++ app/src/pages/Home/Header.jsx
@@ -4,12 +4,17 @@ import { Button } from 'antd';
 export default function Header(props) {
   return (
     <header {...props}>
-      <a className="logo-wrapper" href="https://antv.alipay.com/zh-cn/index.html" target="_blank">
+      <a
+        className="logo-wrapper"
+        href="https://antv.alipay.com/zh-cn/index.html"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
         <i className="logo" />
-        <span>AntV</span>
+        <span>InstantP</span>
       </a>
       <div className="button">
-        <Button>返回旧版</Button>
+        <Button>Call a bot</Button>
       </div>
     </header>
   );
